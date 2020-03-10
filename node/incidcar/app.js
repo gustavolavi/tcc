2d6d8098fe9ed92ce7693a51e0b4cc9f088047cc
vagrant <vagrant@ABE000674-in-IIES272-620-30.seg-social.virt>
2020-03-10 11:27:24 +0000

    node rest

--- node/incidcar/app.js
+++ node/incidcar/app.js
@@ -1,9 +1,7 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-const bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 var usersRouter = require('./src/routes/users');
 
--- node/incidcar/src/routes/users.js
+++ node/incidcar/src/routes/users.js
@@ -4,16 +4,57 @@ var { User } = require('../models');
 
 router.get('/:userId?', (req, res) => {
   const { userId } = req.params;
-  
+
   let query;
-  if(userId) {
-      query = User.findOne({
-        where: { id: Number(userId) }
-      });
+  if (userId) {
+    query = User.findOne({
+      where: { id: Number(userId) }
+    });
   } else {
-      query = User.findAll();
+    query = User.findAll();
   }
-  return query.then(users => res.json(users))
+  return query.then(users => res.json(users));
+})
+
+router.put('/:userId', (req, res) => {
+  const { userId } = req.params;
+  const { name, email, password } = req.body;
+
+  let query = User.update({
+    name: name,
+    email: email,
+    password: password
+  }, {
+      where: {
+        id: userId
+      }
+    });
+
+  return query.then(users => res.json(users));
+})
+
+router.post('/', (req, res) => {
+  const { name, email, password } = req.body;
+
+  let query = User.create({
+    name: name,
+    email: email,
+    password: password
+  });
+
+  return query.then(users => res.json(users));
+})
+
+router.delete('/:userId', (req, res) => {
+  const { userId } = req.params;
+
+  let query = User.destroy({
+    where: {
+      id: userId
+    }
+  });
+
+  return query.then(users => res.json(users));
 })
 
 module.exports = router;
